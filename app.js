e7678c973b13c52d2d77ec792d77ba42b8eaec53
Kegan Myers <kegan@keganmyers.com>
2015-01-10 05:03:33 -0600

    Use library, js refactor

---
+++ app.js
@@ -0,0 +1,54 @@
+(function() {
+    function ReadResult(data) {
+        var transformCache = {};
+        var transformProxy = {
+            _default: function() {
+                return data;
+            },
+            base64: function() {
+                return btoa(data);
+            }
+        };
+        this.transform = function(to) {
+            if (transformProxy.hasOwnProperty(to)) {
+                return transformProxy[to]();
+            }
+            return transformProxy['_default']();
+        }
+    }
+
+    u$.r(function() {
+        document.getElementById((u$.modern ? "" : "un") + "supported").style.display = "block";
+        if (!u$.modern) {
+            return;
+        }
+
+        var fileSelector = document.getElementById("file");
+        var output = document.getElementById("output");
+        var type = document.getElementById("type");
+        var currentResult = null;
+
+        function update() {
+            if (currentResult == null) {
+                output.innerText = "No file loaded.";
+                return;
+            }
+            output.innerText = currentResult.transform(type.value);
+        }
+
+        fileSelector.onchange = function() {
+            var reader = new FileReader();
+            reader.onload = function() {
+                currentResult = new ReadResult(reader.result);
+                update();
+            };
+            reader.readAsBinaryString(fileSelector.files[0]);
+        };
+
+        type.onchange = function() {
+            update();
+        };
+
+        window.u = update;
+    });
+})();
